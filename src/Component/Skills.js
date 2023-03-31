9fb7ebc4c459e5bb1354c7d9658157f73f3ec33c
shahadat10033 <shahadat10033@gmail.com>
2023-03-31 07:44:31 +0600

    displaying 4 card complete

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -10,13 +10,16 @@ const Skills = () => {
   return (
     <div className="col-md-8">
       {values.map((value) => (
-        <div>
-          <div className="d-flex">
-            <div>
-              <div style={{ width: "60px", height: "60px" }}>
+        <div className="border border-black my-3">
+          <div className="mb-3">
+            <img className="img-fluid" src={value.coverImage} alt=""></img>
+          </div>
+          <div className="d-flex justify-content-between my-3">
+            <div className="d-flex justify-content-between my-3">
+              <div style={{ width: "60px", height: "60px" }} className="me-3">
                 <img
-                  className="img-fluid border rounded-5"
-                  src="{value.authorImage}"
+                  className="img-fluid border rounded-circle"
+                  src={value.authorImage}
                   alt=""
                 />
               </div>
@@ -28,12 +31,24 @@ const Skills = () => {
 
             <div>
               <p>
-                <span>{value.readTime}</span> min read <span></span>
+                <span>{value.readTime}</span> min read
+                <button className="border border-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="19"
+                    height="19"
+                    fill="currentColor"
+                    class="bi bi-bookmark"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+                  </svg>
+                </button>
               </p>
             </div>
           </div>
           <h3>{value.title}</h3>
-          <div>
+          <div className="my-5">
             <button className="btn btn-primary">Mark as read</button>
           </div>
         </div>
